0ce83ab3d06592aa08bb7bd961ce03b5f7a86128
NarayanCinnamon <narayan.song@gmail.com>
2025-03-15 15:28:41 -0300

    Sat Mar 15 03:28:41 PM -03 2025 Narayan Silva (narayan.song@gmail.com)

--- etc/skel/.config/polybar/i3-helper.sh
+++ etc/skel/.config/polybar/i3-helper.sh
@@ -14,19 +14,34 @@ cat > "$TMPFILE" <<EOF
 i3 ShortCuts
 
 Mod+Enter  → Open Terminal
+
 Mod+d      → Open Menu (rofi/dmenu)
+
 Mod+b      → Open Browser
+
 Mod+t      → Open Thunar
+
 Mod+Shift+q → Close App/Window
+
 Mod+1-9     → Alt Workspace
+
 Mod+Shift+1-9 → Move app/window to Workspace
+
 Mod+arrows  → Alt Window
+
 Mod+Shift+arrows → Move Window
+
 Mod+e      → Tilling vertical & horizontal
+
 Mod+w      → Tilling like tabs
+
 Mod+f      → Full screen/exit full screen
+
 Mod+Shift+p → Open power manager
+
 Mod+Shift+r → Restart i3
+
+Mod+space   → Change keyboard layout (default options: br, us, ru)
 EOF
 
 # Define o tema escuro (para ajudar com o tema Nordic-darker)
